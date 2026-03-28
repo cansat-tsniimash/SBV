39c807d9de1a1867fa7c13f272000ff8baa2bf76
dfk1r <x6Q7ndcf6Qs2kT>
2026-03-28 18:09:52 +0300

    чтото сделал

--- src/gcs/madgic.py
+++ src/gcs/madgic.py
@@ -1,5 +1,45 @@
 import struct
 
+def lsm6ds3_from_fs2g_to_mg(lsb: int) -> float:
+  return lsb * 61.0 / 1000.0
+
+def lsm6ds3_from_fs4g_to_mg(lsb: int) -> float:
+  return lsb * 122 / 1000.0
+
+def lsm6ds3_from_fs8g_to_mg(lsb: int) -> float:
+  return lsb * 244.0 / 1000.0
+
+def lsm6ds3_from_fs16g_to_mg(lsb: int) -> float:
+  return lsb * 488.0 / 1000.0
+
+def lsm6ds3_from_fs125dps_to_mdps(lsb: int) -> float:
+  return lsb * 4375.0 / 1000.0
+
+def lsm6ds3_from_fs250dps_to_mdps(lsb: int) -> float:
+  return lsb * 8750.0 / 1000.0
+
+def lsm6ds3_from_fs500dps_to_mdps(lsb: int) -> float:
+  return lsb * 1750.0 / 1000.0
+
+def lsm6ds3_from_fs1000dps_to_mdps(lsb: int) -> float:
+  return lsb * 35.0
+
+def lsm6ds3_from_fs2000dps_to_mdps(lsb: int) -> float:
+  return lsb * 70.0
+
+
+def convert(values):
+    values[0],
+    values[1],
+    values[2] / 16,
+    values[3],
+    lsm6ds3_from_fs16g_to_mg(values[4]),
+    lsm6ds3_from_fs16g_to_mg(values[5]),
+    lsm6ds3_from_fs16g_to_mg(values[6]),
+    lsm6ds3_from_fs2000dps_to_mdps(values[7]),
+    lsm6ds3_from_fs2000dps_to_mdps(values[8]),
+    lsm6ds3_from_fs2000dps_to_mdps(values[9]),
+
 class Parser:
     def __init__(self):
         self.lef = bytes()
--- src/gcs/nazemka.py
+++ src/gcs/nazemka.py
@@ -5,10 +5,12 @@ port = serial.Serial("COM4", baudrate=115200)
 port.timeout = 1
 p = madgic.Parser()
 
-with open("telem.bin", mode="wb") as stream:
+with open("telem.bin", mode="wb") as stream, open("chtoto.csv", mode="w") as g :
     while True:
         data = port.read(4096)
         stream.write(data)
         packets = p.parse(data)
         for packet in packets:
-            print(packet)
+            numbers = madgic.convert()
+            g.write(",".join())
+            print(numbers)

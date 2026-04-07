ff5dd5746d7b4fbf1e2bef5ad6dd255e0d641775
KTshka <lebedevaroslav10@gmail.com>
2026-04-07 20:26:28 +0300

    попытка написать регистры ины

--- src/CanSat/INA226/INA226.c
+++ src/CanSat/INA226/INA226.c
@@ -5,4 +5,27 @@
  *      Author: Светлана
  */
 
+#include "stm32f1xx.h"
+#include "INA226.h"
+
+void ina226_write_reg(uint8_t reg_addr, uint8_t reg_data)
+{
+	ina226_bus_t *ina;
+	uint8_t buf[3] = {0};
+	buf[0] = reg_addr;
+	buf[1] = reg_data >> 8;
+	buf[2] = reg_data;
+
+	HAL_I2C_Master_Transmit(ina->hi2c2, ina->addr, buf, 2, 100);
+}
+
+void ina226_read_reg(uint8_t reg_addr, uint16_t data)
+{
+	ina226_bus_t *ina;
+	HAL_I2C_Master_Transmit(ina->hi2c2, ina->addr, data, 2, 100);
+	HAL_I2C_Master_Receive(ina->hi2c2, ina->addr, data, 2, 100);
+
+	return data;
+}
+
 
--- src/CanSat/INA226/INA226.h
+++ src/CanSat/INA226/INA226.h
@@ -7,7 +7,72 @@
 
 #ifndef INA226_H_
 #define INA226_H_
+#include "stm32f1xx.h"
+
+typedef struct
+{
+	uint8_t addr;
+	I2C_HandleTypeDef *hi2c2;
+}ina226_bus_t;
+
+typedef enum
+{
+	AVG_1 = 0,
+	AVG_4 = 1,
+	AVG_16 = 2,
+	AVG_64 = 3,
+	AVG_128 = 4,
+	AVG_256 = 5,
+	AVG_512 = 6,
+	AVG_1024 = 7
+
+}ina226_avg_t;
+
+typedef enum
+{
+	VBUSCT_140US = 0,
+	VBUSCT_204US = 1,
+	VBUSCT_332US = 2,
+	VBUSCT_588US = 3,
+	VBUSCT_1p1MS = 4,
+	VBUSCT_2p116MS = 5,
+	VBUSCT_4p156 = 6,
+	VBUSCT_8p244MS = 7
+
+}ina226_vbusct_t;
+
+typedef enum
+{
+	VSHCT_140US = 0,
+	VSHCT_204US = 1,
+	VSHCT_332US = 2,
+	VSHCT_588US = 3,
+	VSHCT_1p1MS = 4,
+	VSHCT_2p116MS = 5,
+	VSHCT_4p156 = 6,
+	VSHCT_8p244MS = 7
+
+}ina226_vshct_t;
+
+typedef enum
+{
+	OP_MODE_SHUTDOWN = 0,
+	OP_MODE_SHUNTnVOLT_TRIGGERED = 1,
+	OP_MODE_BUSnVOLT_TRIGGERED = 2,
+	OP_MODE_SHUNTnBUS_TRIGGERED = 3,
+	//OP_MODE_SHUTDOWN = 4,
+	OP_MODE_SHUNTnVOLT = 5,
+	OP_MODE_BUSnVOLT = 6,
+	OP_MODE_SHUNTnBUS = 7
+
+}ina226_operatig_mode_t;
+
+
+
+
+
 
 
 
 #endif /* INA226_H_ */
+

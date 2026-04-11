619d004ccc8422b47af2d0cdae54e9db8543377a
dfk1r <x6Q7ndcf6Qs2kT>
2026-04-11 16:53:49 +0300

    подправил попугаев

--- src/gcs/madgic.py
+++ src/gcs/madgic.py
@@ -29,13 +29,46 @@ def lsm6ds3_from_fs2000dps_to_mdps(lsb: int) -> float:
 # какиета нужные функции
 
 def convert_std(values):
-    return values
+    return (
+        values[1],
+        values[2],
+        values[3] / 100,
+        values[4],
+        lsm6ds3_from_fs16g_to_mg(values[5]),
+        lsm6ds3_from_fs16g_to_mg(values[6]),
+        lsm6ds3_from_fs16g_to_mg(values[7]),
+        lsm6ds3_from_fs2000dps_to_mdps(values[8]),
+        lsm6ds3_from_fs2000dps_to_mdps(values[9]),
+        lsm6ds3_from_fs2000dps_to_mdps(values[10]),
+    )
 # превращаем попугаев в нужные еденицы измерения
 
 
 def convert_our(values):
     return (
-        *values,
+        values[0],
+        values[1],
+        values[2],
+        values[3],
+        values[4],
+        values[5],
+        values[6],
+        values[7],
+        values[8],
+        values[9],
+        values[10],
+        values[11],
+        values[12],
+        values[13],
+        values[14],
+        values[15],
+        values[16] / 10,
+        lsm6ds3_from_fs2000dps_to_mdps(values[17]),
+        lsm6ds3_from_fs2000dps_to_mdps(values[18]),
+        lsm6ds3_from_fs2000dps_to_mdps(values[19]),
+        lsm6ds3_from_fs16g_to_mg(values[20]),
+        lsm6ds3_from_fs16g_to_mg(values[21]),
+        lsm6ds3_from_fs16g_to_mg(values[22]),
     )
 
 class Parser: # подклас библеотеки
@@ -47,29 +80,28 @@ class Parser: # подклас библеотеки
         rv = []
         data = self.lef + data
 
-        while len(data) >= 80: # если не достаточно данных то не работает
+        while len(data) >= 81: # если не достаточно данных то не работает
             while len(data) > 2 and (data[0] != 0xaa or data[1] != 0xaa):
                 data = data[1:]
             # находим метку начала пакета
-            if len(data) < 80:
+            if len(data) < 81:
                 break
 
-            packet = data[2:27] # отсикаем метку начала пакета и тим айди
+            packet = data[:27] # отсикаем метку начала пакета
             chk = packet[0]
             for b in packet[1:]:
                 chk = chk ^ b
             if chk == 0: # сверяем контрольную сумму
-                values = struct.unpack("<HIhI3h3hB", packet) # рашифровываем основную часть покета
+                values = struct.unpack("<HHIhI3h3hB", packet) # рашифровываем основную часть покета
                 data = data[28:] # перенсим в нашу часть покета
-                copi = data[0:52]
+                copi = data[0:53]
                 chk = copi[0]
                 for o in copi[1:]:
                     chk = chk ^ o
-                chk = 0 # PODSTAVA!!!!!!!!
                 if chk == 0: # контрольная сумма нашей части пакета
-                    orig = struct.unpack("<HB3f6Hf10HB", copi) # рашифровка нашей части пакета
+                    orig = struct.unpack("<HB3fB6Hf10HB", copi) # рашифровка нашей части пакета
                     rv.append((values, orig,)) # записваем в масив
-                    data = data[53:] # берём другую часть пакета
+                    data = data[54:] # берём другую часть пакета
                 else: # если что то пошло не так
                     rv.append((values, None,))  # записваем в масив
                     data = data[1:]

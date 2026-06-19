8d8e9c28917e3aaf7567af2d5dea6ce8d91edf94
dfk1r <x6Q7ndcf6Qs2kT>
2026-06-19 18:00:32 +0300

    Что-то сломалось

--- src/gcs/madgic.py
+++ src/gcs/madgic.py
@@ -26,25 +26,30 @@ def lsm6ds3_from_fs1000dps_to_mdps(lsb: int) -> float:
 
 def lsm6ds3_from_fs2000dps_to_mdps(lsb: int) -> float:
   return lsb * 70.0
-# какиета нужные функции
+
+def lis2mdl_from_lsd_to_mgauss(lsb: int) -> float:
+    return lsb * 1.5
+# какиета нужные функции для акселерометра и магнетометра
 
 def convert_std(values):
+    # return values
     return (
         values[1],
         values[2],
         values[3] / 100,
         values[4],
-        lsm6ds3_from_fs16g_to_mg(values[5]),
-        lsm6ds3_from_fs16g_to_mg(values[6]),
-        lsm6ds3_from_fs16g_to_mg(values[7]),
-        lsm6ds3_from_fs2000dps_to_mdps(values[8]),
-        lsm6ds3_from_fs2000dps_to_mdps(values[9]),
-        lsm6ds3_from_fs2000dps_to_mdps(values[10]),
+        lsm6ds3_from_fs16g_to_mg(values[5]) / 1000,
+        lsm6ds3_from_fs16g_to_mg(values[6]) / 1000,
+        lsm6ds3_from_fs16g_to_mg(values[7]) / 1000,
+        lsm6ds3_from_fs2000dps_to_mdps(values[8]) / 1000,
+        lsm6ds3_from_fs2000dps_to_mdps(values[9]) / 1000,
+        lsm6ds3_from_fs2000dps_to_mdps(values[10]) / 1000,
     )
-# превращаем попугаев в нужные еденицы измерения
+# функцией превращаем попугаев в нужные еденицы измерения части аргонизаторов
 
 
 def convert_our(values):
+    # return values
     return (
         values[0],
         values[1],
@@ -52,26 +57,34 @@ def convert_our(values):
         values[3],
         values[4],
         values[5],
-        values[6],
-        values[7],
-        values[8],
-        values[9],
-        values[10],
-        values[11],
+        values[6] / 1000,
+        values[7] / 1000,
+        values[8] / 1000,
+        values[9] / 1000,
+        values[10] / 1000,
+        values[11] / 1000,
         values[12],
         values[13],
         values[14],
         values[15],
-        values[16] / 10,
-        lsm6ds3_from_fs2000dps_to_mdps(values[17]),
-        lsm6ds3_from_fs2000dps_to_mdps(values[18]),
-        lsm6ds3_from_fs2000dps_to_mdps(values[19]),
-        lsm6ds3_from_fs16g_to_mg(values[20]),
-        lsm6ds3_from_fs16g_to_mg(values[21]),
-        lsm6ds3_from_fs16g_to_mg(values[22]),
+        lis2mdl_from_lsd_to_mgauss(values[16]),
+        lis2mdl_from_lsd_to_mgauss(values[17]),
+        lis2mdl_from_lsd_to_mgauss(values[18]),
+        values[19] / 10,
+        lsm6ds3_from_fs2000dps_to_mdps(values[20]) / 1000,
+        lsm6ds3_from_fs2000dps_to_mdps(values[21]) / 1000,
+        lsm6ds3_from_fs2000dps_to_mdps(values[22]) / 1000,
+        lsm6ds3_from_fs16g_to_mg(values[23]) / 1000,
+        lsm6ds3_from_fs16g_to_mg(values[24]) / 1000,
+        lsm6ds3_from_fs16g_to_mg(values[25]) / 1000,
+        values[26],
     )
+# функцией превращаем попугаев в нужные еденицы измерения части
 
 class Parser: # подклас библеотеки
+    STRUCT_STD = struct.Struct("<HHIhI3h3hB")
+    STRUCT_OUR = struct.Struct("<HB3fB6H2h2h10hfB")
+
     def __init__(self):
         self.lef = bytes()
 
@@ -80,28 +93,28 @@ class Parser: # подклас библеотеки
         rv = []
         data = self.lef + data
 
-        while len(data) >= 81: # если не достаточно данных то не работает
+        while len(data) >= self.STRUCT_STD.size + self.STRUCT_OUR.size: # если не достаточно данных то не работает
             while len(data) > 2 and (data[0] != 0xaa or data[1] != 0xaa):
                 data = data[1:]
             # находим метку начала пакета
-            if len(data) < 81:
+            if len(data) < self.STRUCT_STD.size + self.STRUCT_OUR.size:
                 break
 
-            packet = data[:27] # отсикаем метку начала пакета
+            packet = data[:self.STRUCT_STD.size] # отсикаем метку начала пакета
             chk = packet[0]
             for b in packet[1:]:
                 chk = chk ^ b
             if chk == 0: # сверяем контрольную сумму
-                values = struct.unpack("<HHIhI3h3hB", packet) # рашифровываем основную часть покета
-                data = data[28:] # перенсим в нашу часть покета
-                copi = data[0:53]
+                values = self.STRUCT_STD.unpack(packet) # рашифровываем основную часть покета
+                data = data[self.STRUCT_STD.size:] # перенсим в нашу часть покета
+                copi = data[0:self.STRUCT_OUR.size]
                 chk = copi[0]
                 for o in copi[1:]:
                     chk = chk ^ o
                 if chk == 0: # контрольная сумма нашей части пакета
-                    orig = struct.unpack("<HB3fB6Hf10HB", copi) # рашифровка нашей части пакета
+                    orig = self.STRUCT_OUR.unpack(copi) # рашифровка нашей части пакета
                     rv.append((values, orig,)) # записваем в масив
-                    data = data[54:] # берём другую часть пакета
+                    data = data[self.STRUCT_OUR.size:] # берём другую часть пакета
                 else: # если что то пошло не так
                     rv.append((values, None,))  # записваем в масив
                     data = data[1:]
--- src/gcs/nazemka.py
+++ src/gcs/nazemka.py
@@ -2,21 +2,33 @@ import serial # импортируем библиотеку и фаил с ос�
 import madgic
 from madgic import Parser, convert_std, convert_our
 
-port = serial.Serial("COM4", baudrate=9600) # выбираем порт и скорость данных по юарту
-port.timeout = 1
-p = madgic.Parser()
-
-tim = "Tim ID"
-time = "Время"
-c = "Температура"
-d = "Давление"
-vx = "Ускорение по x"
-vy = "Ускорение по y"
-vz = "Ускорение по z"
-xx = "Угловая скорость по x"
-xy = "Угловая скорость по y"
-xz = "Угловая скорость по z"
+headers_std = ("Tim ID", "   time", " temp", "     P", "   a[x]", "  a[y]", "  a[z]", " w[x]", " w[y]", " w[z]",)
+headers_our = (
+    "  num", "st", "   lat", "   lon", "     h", "  fx",
+    "  ph[0]", "  ph[1]", "  ph[2]", "  ph[3]", "  ph[4]", "  ph[5]",
+    " bus1", "shunt1", " bus2", "shunt2",
+    " mag[x]", " mag[y]", " mag[z]", " temp", "  a2[x]", "  a2[y]", "  a2[z]", " w2[x]", " w2[y]", " w2[z]", "y_p"
+)
 #ввожу переменые для названий заголовков столбов
+print(headers_std, headers_our, sep="")
+
+def print_aligned(headers, values):
+#    print(*headers)
+    for no, header in enumerate(headers):
+        value = values[no]
+        if isinstance(value, float):
+            value = "%.2f" % value
+        else:
+            value = str(value)
+
+        value = " " * (len(header) - len(value)) + value
+        print(value, end=" ")
+
+    print()
+
+port = serial.Serial("COM4", baudrate=9600) # выбираем порт и скорость данных по юарту
+port.timeout = 0.05
+p = madgic.Parser()
 
 with open("telem.bin", mode="wb") as stream, open("chtoto.csv", mode="w") as g : # записывает чтото там
     while True:
@@ -29,12 +41,11 @@ with open("telem.bin", mode="wb") as stream, open("chtoto.csv", mode="w") as g :
                 continue
 
             numbers = convert_std(std)
-            g.write((";".join(str(x).replace(".", ",") for x in numbers) + "\n")) #меняю точки на запетые для иксель
-            print(tim, time, c, d, vx, vy, vz, xx, xy, xz, "\n", numbers, end="") # записываем отредоктированые пакеты
-
+            #g.write((";".join(str(x).replace(".", ",") for x in numbers) + "\n")) #меняю точки на запетые для иксель
             if our is None:
-                print("BAD OUR CHECKSUM")
+                print( "BAD OUR CHECKSUM")
+                print_aligned(headers_std, numbers)
                 continue
 
             numbers2 = convert_our(our)
-            print(numbers2)
+            print_aligned(headers_std + headers_our, numbers + numbers2)
